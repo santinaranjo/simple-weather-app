e78e3ba9a5511b8c5cf3f5ee5098dd8fe7a2cfc2
santinaranjo <marketingsanti@gmail.com>
2022-05-02 17:40:29 +0200

    Función getData modularizada

---
+++ components/getData.js
@@ -0,0 +1,14 @@
+const baseUrlAPI = "https://api.openweathermap.org/";
+const apiKEY = "e24af0c56b139eaf9284353c8813905c";
+
+async function getData(city) {
+    const response = await fetch(`${baseUrlAPI}data/2.5/weather?q=${city.value}&appid=${apiKEY}&lang=sp`);
+
+    if (response.status === 200) {
+        return response;
+    } else {
+        return false;
+    };
+};
+
+export default getData;
