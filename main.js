dfca2ed1cb73ba8b2d1ae37fbf970ceb2142bc65
santinaranjo <marketingsanti@gmail.com>
2022-05-02 18:36:25 +0200

    Creada estructura basica de tarjetas

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
-import getData from "./components/getData";
+import getData from "./components/getData.js";
 
 const inputField = document.querySelector(".main__input--text");
 const button = document.querySelector(".main__input--button");
 const results = document.querySelector(".main__results");
 
+
+
+async function createCard() {
+    const response = await getData(inputField);
+
+    if (response) {
+        const weatherData = response.json()
+            .then(
+                function (data) {
+                    const cardContent = `
+                        <div class="card__title">${data.name}</div>
+                        <span class="card__temp">${data.main.temp}</span>
+                        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">
+                        <span class="card__weather">${data.weather[0].description}</span>
+                    `;
+                    results.innerHTML = cardContent;
+                })
+            .catch(err => console.log(err))
+    } else {
+        inputField.value = "";
+        inputField.placeholder = "Ha habido un error";
+    }
+}
+
+
+
+
 // async function getData() {
 //     const city = inputField.value;
 //     const response = await fetch(`${baseUrlAPI}data/2.5/weather?q=${city}&appid=${apiKEY}&lang=sp`);
 
-//     if (response.status === 200) {
-//         const weatherData = response.json()
-//             .then(
-//                 function (data) {
-//                     results.innerHTML = `${data.weather[0].description}`;
-
-//                 })
-//             .catch(err => console.log(err))
-//     } else {
-//         inputField.value = "";
-//         inputField.placeholder = "Ha habido un error";
-//     }
-
+//     
 // }
 
 
 
-button.addEventListener("click", getData);
+button.addEventListener("click", createCard);
