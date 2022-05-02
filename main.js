c666e62a7a04d93676120bf1aaa3f7f385f19be9
santinaranjo <marketingsanti@gmail.com>
2022-05-02 19:34:24 +0200

    Logica terminada

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const button = document.querySelector(".main__input--button");
 const results = document.querySelector(".main__results");
 
 
-
 async function createCard() {
     const response = await getData(inputField);
 
@@ -13,13 +12,21 @@ async function createCard() {
         const weatherData = response.json()
             .then(
                 function (data) {
+                    const cardContainer = document.createElement("div");
                     const cardContent = `
-                        <div class="card__title">${data.name}</div>
+                        <div class="card__title">
+                            <span>${data.name}</span>
+                            <span>${data.sys.country}</span>
+                        </div>
                         <span class="card__temp">${data.main.temp}</span>
                         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">
                         <span class="card__weather">${data.weather[0].description}</span>
                     `;
-                    results.innerHTML = cardContent;
+                    
+                    cardContainer.className = "card__container";
+                    cardContainer.innerHTML = cardContent;
+                    results.append(cardContainer);
+                    inputField.value = "";
                 })
             .catch(err => console.log(err))
     } else {
@@ -27,16 +34,6 @@ async function createCard() {
         inputField.placeholder = "Ha habido un error";
     }
 }
-
-
-
-
-// async function getData() {
-//     const city = inputField.value;
-//     const response = await fetch(`${baseUrlAPI}data/2.5/weather?q=${city}&appid=${apiKEY}&lang=sp`);
-
-//     
-// }
 
 
 
